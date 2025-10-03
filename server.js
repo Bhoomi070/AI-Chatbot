82b88ebc76f78e8181ace9bd0e6ef1ee9c4ca36f
Bhoomi Sharma <bhoomi3100@gmail.com>
2025-10-03 17:29:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const app = require('./src/app');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/service/ai.service");
+
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
+io.on("connection",(socket)=>{
+    console.log("A user connected");
+
+    socket.on("disconnect",()=>{
+        console.log("A user disconnect")
+    });
+
+    socket.on("ai-message",async (data)=>{
+        console.log("Received AI message:", data.prompt);
+        const response = await generateResponse(data.prompt);
+        console.log("Ai Response:" , response);
+        // socket.emit("ai-message-response",{response})
+    })
+});
+
+httpServer.listen(3000, () =>{
+    console.log("server is running on port 3000");
+})
+
+
+//event are of two types : inbuild event and custom event
